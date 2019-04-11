e18d39641e18882e5311c4ffb628aa7809332b5e
c212 <40395648+c212@users.noreply.github.com>
2019-04-11 14:05:50 -0400

    three cases out of four
    the real case still has to be addressed

--- hw10/Sequence.java
+++ hw10/Sequence.java
@@ -13,10 +13,17 @@ public class Sequence extends ArrayList<Integer> {
     Sequence b = new Sequence(); 
     Sequence result = b.merge(a); 
     System.out.println( "merge(" + b + ", " + a + ") = " + result ); 
+    result = a.merge(b); 
+    System.out.println( "merge(" + a + ", " + b + ") = " + result ); 
+    a = new Sequence();
+    b = new Sequence(); 
+    result = a.merge(b); 
+    System.out.println( "merge(" + a + ", " + b + ") = " + result ); 
   }
   // if a and b are sortted the result is also sorted 
   public Sequence merge(Sequence other) {
     if (this.size() == 0) return other; 
+    else if (other.size() == 0) return this; 
     else return null; 
   }
 }
