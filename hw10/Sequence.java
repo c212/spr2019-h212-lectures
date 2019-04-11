cb95b16d1b635dda052c21269098b82a1cb299ff
c212 <40395648+c212@users.noreply.github.com>
2019-04-11 13:45:56 -0400

    chose inheritance over composition
    let me see how it goes

--- hw10/Sequence.java
+++ hw10/Sequence.java
@@ -1,21 +1,11 @@
 import java.util.ArrayList; 
 
-public class Sequence {
-  private ArrayList<Integer> values; 
-  public Sequence() {
-    this.values = new ArrayList<Integer>();
-  }
+public class Sequence extends ArrayList<Integer> {
+  public Sequence() {  }
   public Sequence(int[] values){ 
-    this(); // ha, ha!
     for (int v : values)
-      this.values.add( v);
+      this.add( v);
   } 
-  public void add(int n) {
-    this.values.add(n);
-  }
-  public String toString() {
-    return this.values.toString();
-  }
   public static void main(String[] args) {    
     Sequence w = new Sequence( new int[] { 4, 5, 2, 3, 1, 7, 6 } ); 
               // notice how easy it is to initialize a Sequence now
